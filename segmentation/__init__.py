8e284642b6780566af866830f1afd403e49fbba9
reynaldo <graducsb@gmail.com>
2021-01-10 17:20:05 -0700

    changing dir structure

--- segmentation/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from .models import unet, xnet, build_classifier
-from .rle_utils import *
-from .augmentations import *
-from .metrics_and_losses import *
--- segmentation/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .xnet import xnet
-from .unet import unet
-from .classification_model import build_classifier
